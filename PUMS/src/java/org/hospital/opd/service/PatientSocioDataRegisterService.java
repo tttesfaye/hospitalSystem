ef3d353a1f2d84ce8f7a0ec58b010a14a837b084
Teklu <teklutesfayeh@gmail.com>
2016-02-27 10:15:48 +0300

    add service class to show how commit can be done

--- PUMS/src/java/org/hospital/opd/service/PatientSocioDataRegisterService.java
+++ PUMS/src/java/org/hospital/opd/service/PatientSocioDataRegisterService.java
@@ -11,5 +11,11 @@ package org.hospital.opd.service;
  * @author abebe
  */
 public class PatientSocioDataRegisterService {
-    
+    public static void main(String [] args){
+        int a;
+        int c=8;
+        int d =10;
+       
+    //   System.out.println(" the valud of a is "+ a);
+    }
 }
